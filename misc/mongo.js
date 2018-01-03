3f793cb12d24645c5d5a8093079858171699dd10
kennyhei <kenny_heinonen@hotmail.com>
2018-01-03 21:55:05 +0200

    Add sandbox file for testing stuff

--- misc/mongo.js
+++ misc/mongo.js
@@ -1,8 +1,23 @@
 const mongoose = require('mongoose')
 
-const url = 'mongodb://heroku_nzqmcb5c:6jos0k8mb222gi3hhl9osbl8bn@ds237707.mlab.com:37707/heroku_nzqmcb5c'
+if (process.env.NODE_ENV !== 'production') {
+    let envPath = __dirname.split('\\')
+    envPath.pop()
+    envPath = envPath.join('\\') + '\\.env'
 
-mongoose.connect(url)
+    require('dotenv').config({ path: envPath })
+}
+  
+const url = process.env.MONGODB_URI
+
+console.log(url)
+
+mongoose.connect(url).then(result => {
+    console.log(result)
+})
+.catch(error => {
+    console.log(error)
+})
 mongoose.Promise = global.Promise
 
 const Note = mongoose.model('Note', {
@@ -11,8 +26,8 @@ const Note = mongoose.model('Note', {
     important: Boolean
 })
 
-/*const note = new Note({
-    content: 'HTTP-protokollan tärkeimmät metodit ovat GET ja POST',
+const note = new Note({
+    content: 'Rakastan muistiinpanoja',
     date: new Date(),
     important: true
 })
@@ -21,11 +36,11 @@ note.save().then(result => {
     console.log(result)
     console.log('note saved')
     mongoose.connection.close()
-})*/
+})
 
-Note.find({ important: true }).then(result => {
+/*Note.find({ important: true }).then(result => {
     result.forEach(note => {
         console.log(note)
     })
     mongoose.connection.close()
-})
+})*/
