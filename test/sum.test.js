b0f2c356d2d7b507d77bca7c91fda0b520bf2831
kennyhei <kenny_heinonen@hotmail.com>
2018-01-04 21:34:45 +0200

    Add jest

---
+++ test/sum.test.js
@@ -0,0 +1,12 @@
+/**
+ * MOCHA TEST
+
+const assert = require('assert');
+const sum = require('../utils/sum')
+
+describe('sum', () => {
+    it('should return 5 for 2 + 3', () => {
+      const result = sum(2, 3)
+      assert.equal(result, 5);
+    });
+});*/
